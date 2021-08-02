eb7af2ab2bb215c035afb70e56beb995eece6708
Mohammad Rahmani <83430369+MohammadRK2000@users.noreply.github.com>
2021-08-01 23:58:05 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+var JSSoup = require('jssoup').default;
+
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+
+
+const Table = require('cli-table');
+
+
+
+let users = [];
+let table = new Table({
+    head: ['username', 'likes', 'challenges'],
+    colWidths: [15, 5, 10]
+
+})
+
+
+const options = {
+    url: `https://forum.freecodecamp.org/directory_items?period=weekly&order=likes_received`
+    , json: true
+
+
+}
+
+rp(options)
+    .then((data) => {
+        let promises = [];
+        let userData = [];
+        for (let user of data.directory_items) {
+            userData.push({ name: user.user.username, likes_received: user.likes_received });
+        }
+
+        process.stdout.write('loading')
+        getChallengesCompletedAndPushToUserArray(userData);
+
+
+    })
+    .catch((err) => {
+        console.log(err)
+    });
+
+function getChallengesCompletedAndPushToUserArray(userData) {
+
+    var i = 0;
+    function next() {
+        if (i < userData.length) {
+            var options = {
+                url: `https://forum.freecodecamp.org/u/` + userData[i].name,
+                transform: body => cheerio.load(body)
+
+            }
+
+
+            rp(options)
+                .then(function ($) {
+                    process.stdout.write(`.`)
+                    const fccAccount = $('h1.landing-heading').length == 0;
+                    const challengesPassed = fccAccount ? $('tbody tr').length : 'unknown';
+                    table.push([userData[i].name, userData[i].likes_received, challengesPassed]);
+                    ++i;
+                    return next();
+
+                })
+
+        }
+        else {
+            printData();
+
+        }
+    }
+    return next();
+};
+
+function printData() {
+    console.log("check");
+    console.log(table.toString());
+
+}
